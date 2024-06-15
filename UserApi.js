08519b18f59968d310eab37a4c34aa987f64cb61
miriam <miriammugure610@gmail.com>
2024-06-15 16:35:16 +0300

    add the corrected files

--- UserApi.js
+++
@@ -1,9 +0,0 @@
-const UserApi = async () => {
-  const response = await fetch("https://api.github.com/users/miriammugure");
-  if (!response.ok) {
-    return "Network response was not ok";
-  }
-  return response.json();
-};
-
-export default UserApi;
